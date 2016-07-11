8ecb1f4fde3882ab7a9f4c13e178741909dc8618
Jan Pecha <janpecha@email.cz>
2016-07-11 08:44:42 +0200

    Query: iterating of Query object throws exception

---
+++ src/Exception/NotImplementedException.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This file is part of the Inlm\QueryObject extension
+ * for the Lean Mapper library (http://leanmapper.com)
+ */
+
+namespace Inlm\QueryObject\Exception;
+
+class NotImplementedException extends Exception
+{
+}
